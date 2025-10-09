41cee0dcd39cce8a2a8787a2940b4c49a483d14c
NoahCJames <noahcj500@gmail.com>
2025-10-09 11:07:42 -0400

    Working on making accessible for phone

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 //Used zybooks day 3-3 reading, 10.1: Document Object Model
 let header = document.querySelector("header");
-header.innerHTML = "<h1 style='color=white; margin-top=1vh';>Noah James</h1>";
+header.innerHTML = "<h1 style='color=white; margin-top=1vh;'>Noah James</h1>";
 
 let h2 = document.querySelector("h2");
 h2.innerHTML = '<p><a aria-label="Link to Home page" href="./index.html" id="nav">Home</a></p> <p><a aria-label="Link to Resume page" href="./resume.html" id="nav">Resume</a></p> <p><a aria-label="Link to Portfolio page" href="./portfolio.html" id="nav">Portfolio</a></p>';
